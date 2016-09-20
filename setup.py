fd2f5bc5b297fbfe54c275b8da3713ff2bc208e6
Jim McCusker <mccusker@gmail.com>
2016-09-20 09:32:37 -0600

    Added pypi uploads and examples.

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
@@ -25,14 +25,17 @@ setup(
         'pandas',
         'requests',
         'toposort',
-        'requests-testadapter'
+        'beautifulsoup4',
+        'jinja2',
+        'requests-testadapter',
     ],
     entry_points = {
         'console_scripts': ['setlr=setlr:main'],
     },
+    packages = find_packages(),
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Utilities",
-        "License :: OSI Approved :: Apache 2.0 License",
+        "License :: OSI Approved :: Apache Software License",
     ],
 )
