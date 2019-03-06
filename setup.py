0b4b76f97921cce58273e541d716d3ae872dc22e
Jim McCusker <mccusker@gmail.com>
2019-03-06 10:06:21 -0500

    Updating version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name = "setlr",
-    version = "0.2.11",
+    version = "0.2.12",
     author = "Jim McCusker",
     author_email = "mccusj@cs.rpi.edu",
     description = ("setlr is a tool for Semantic Extraction, Transformation, and Loading."),
