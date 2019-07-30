bf3ad4f0a2814a7564f4ee5f0f4c74da19fab99a
tlprotzman <tlprotzman+github@gmail.com>
2019-07-30 21:08:08 +0000

    VAX compatibility

--- tests/setlr_test/test_read_json.py
+++ tests/setlr_test/test_read_json.py
@@ -10,13 +10,13 @@ class TestReadJson(unittest.TestCase):
     def test_read_json(self):
         expected_string = ''
         root_dir_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
-        json_file = root_dir_path + "/tests/setlr_test/test_read_json.json"
+        json_file = os.path.join(root_dir_path, "tests", "setlr_test", "test_read_json.json")
         with open(json_file) as f:
             expected_string = f.read()
         expected_json = json.loads(expected_string)
         
         result = read_json(("file://{}".format(json_file)), rdflib.resource.Resource(rdflib.graph.Graph(), "test_read_json"))
-        result = [r for r in result]      
+        result = list(result)
 
         self.assertCountEqual(expected_json, result[0][1], "JSON objects not equal")
 
