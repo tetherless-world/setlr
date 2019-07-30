0a314e4ef07d1335019d90dcd7c6edd99910f3f1
tlprotzman <tlprotzman+github@gmail.com>
2019-07-30 21:00:57 +0000

    CWD independent

--- tests/setlr_test/test_read_json.py
+++ tests/setlr_test/test_read_json.py
@@ -1,6 +1,7 @@
 import unittest
 import rdflib
 import json
+import os
 
 from setlr import read_json
 
@@ -8,7 +9,8 @@ from setlr import read_json
 class TestReadJson(unittest.TestCase):
     def test_read_json(self):
         expected_string = ''
-        json_file = "tests/setlr_test/test_read_json.json"
+        root_dir_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+        json_file = root_dir_path + "/tests/setlr_test/test_read_json.json"
         with open(json_file) as f:
             expected_string = f.read()
         expected_json = json.loads(expected_string)
