db5c1468515d9c231e367985fed4c12deeef6ea6
Jamie McCusker <mccusj2@rpi.edu>
2024-08-09 18:09:16 -0400

    Tagging version 1.0.1

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 import os
 from setuptools import setup, find_packages
-from setlr._version import __version__
+from sys import argv
+#from _version import __version__
+
+__version__='1.0.1'
 
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
@@ -9,45 +12,49 @@ from setlr._version import __version__
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-setup(
-    name = "setlr",
-    version = __version__,
-    author = "Jamie McCusker",
-    author_email = "mccusj@cs.rpi.edu",
-    description = ("setlr is a tool for Semantic Extraction, Transformation, and Loading."),
-    license = "Apache License 2.0",
-    keywords = "rdf semantic etl",
-    url = "http://packages.python.org/setlr",
-    packages=['setlr'],
-    long_description='''SETLr is a tool for generating RDF graphs, including named graphs, from almost any kind of tabular data.''',
-    include_package_data = True,
-    install_requires = [
-        'future',
-        'pip>=9.0.0',
-        'cython',
-        'numpy',
-        'rdflib>=6.0.0',
-        'pandas>=0.23.0',
-        'requests',
-        'toposort',
-        'beautifulsoup4',
-        'jinja2',
-        'lxml',
-        'six',
-        'xlrd',
-        'ijson',
-        'click',
-        'tqdm',
-        'requests-testadapter',
-        'python-slugify',
-        'pyshacl[js]'
-    ],
-    entry_points = {
-        'console_scripts': ['setlr=setlr:main'],
-    },
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Topic :: Utilities",
-        "License :: OSI Approved :: Apache Software License",
-    ],
-)
+if '--version' in argv:
+    print(__version__)
+else:
+    setup(
+        name = "setlr",
+        version = __version__,
+        author = "Jamie McCusker",
+        author_email = "mccusj@cs.rpi.edu",
+        description = ("setlr is a tool for Semantic Extraction, Transformation, and Loading."),
+        license = "Apache License 2.0",
+        keywords = "rdf semantic etl",
+        url = "http://packages.python.org/setlr",
+        packages=['setlr'],
+        long_description='''SETLr is a tool for generating RDF graphs, including named graphs, from almost any kind of tabular data.''',
+        include_package_data = True,
+        install_requires = [
+            'future',
+            'pip>=9.0.0',
+            'cython',
+            'numpy',
+            'rdflib>=6.0.0',
+            'pandas>=0.23.0',
+            'requests',
+            'toposort',
+            'beautifulsoup4',
+            'jinja2',
+            'lxml',
+            'six',
+            'xlrd',
+            'ijson',
+            'click',
+            'tqdm',
+            'requests-testadapter',
+            'python-slugify',
+            'pyshacl[js]'
+        ],
+        entry_points = {
+            'console_scripts': ['setlr=setlr:main'],
+        },
+        classifiers=[
+            "Development Status :: 5 - Production/Stable",
+            "Topic :: Utilities",
+            "License :: OSI Approved :: Apache Software License",
+        ],
+    )
+
